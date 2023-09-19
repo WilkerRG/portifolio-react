ff99bb4678a1230622d9123f5211773ae66a6dec
Wilker <wilkerrguimaraes@hotmail.com>
2023-09-19 10:56:21 -0300

    Home feita

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,11 +1,29 @@
 import './home.modules.css';
 import Footer from '../componets/footer';
 import Header from '../componets/header';
+import device from './Devices-amico.png';
 
 function Home() {
   return (
-    // <Header/>
-    //  <Footer/>
+    <div className="projeto">
+      <Header />
+      <div className="Main">
+        <div className='textContainer'>
+          <h4>Ola, meu nome é</h4>
+          <h1>Wilker <span>Guimarães</span></h1>
+          <h3>Eu sou um estudante de desenvolvimento web</h3>
+        </div>
+
+        <div className='imageContainer'>
+          <img src={device} />
+        </div>
+
+      </div>
+
+
+      <Footer />
+    </div >
+
   );
 }
 
---
+++ src/Sobre/sobre.js
@@ -0,0 +1,17 @@
+function Sobre() {
+    return (
+      <div className="projeto">
+        <Header />
+        <div className="Main-sobre">
+
+        </div>
+  
+  
+        <Footer />
+      </div >
+  
+    );
+  }
+  
+  export default Sobre;
+  
